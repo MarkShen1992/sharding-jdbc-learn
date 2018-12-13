827746abbf8d442d2d1029173dd79c2efe7fd4e9
Mark <18009838166@163.com>
2018-12-13 18:54:27 +0800

    fix program bug about connection string.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sharding-jdbc-learn
-learn sharding jdbc
+1. 数据库连接串
+
+| 数据库类型 | 连接串                                                       |
+| ---------- | ------------------------------------------------------------ |
+| MySQL      | [ com.mysql.jdbc.Driver ] jdbc:mysql://localhost:3306/sample_db |
+| Mariadb    | [org.mariadb.jdbc.Driver] jdbc:mariadb://localhost:3306/test |
+| PostgreSQL | [org.postgresql.Driver]  jdbc:postgresql://localhost:5432/pgsqltest |
+| h2         | [org.h2.Driver]  jdbc:h2:tcp://localhost/~/mini-web          |
+| mongodb    | MongoDB Java Driver                                          |
+| redis      | Jedis                                                        |
+| ssdb       | https://github.com/ssdb/javassdb                             |
+
---
+++ demo0100/sql/users.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MariaDB
+ Source Server Type    : MariaDB
+ Source Server Version : 100307
+ Source Host           : localhost:3306
+ Source Schema         : test1
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100307
+ File Encoding         : 65001
+
+ Date: 13/12/2018 18:53:02
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `userName` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户名',
+  `passWord` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
+  `user_sex` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `nick_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
